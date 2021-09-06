7221a90ef362ef5031b74b1ec1bf55b10895331d
Alexandru <videoloss3@gmail.com>
2021-09-06 21:16:58 +0300

    Module rewrite from drupal-ce template

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: '@nuxt/test-utils',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'lib/**/*.js',
+    '!lib/plugin.js'
+  ],
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/lib/$1',
+    '^~~$': '<rootDir>',
+    '^@@$': '<rootDir>',
+    '^@/(.*)$': '<rootDir>/lib/$1'
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
+}
